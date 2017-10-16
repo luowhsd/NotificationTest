4fbf8c5c4fcfca3ab5b2648e7d4b44144bb40196
luowh <luowh@chinasap.cn>
2017-10-16 14:01:21 +0800

    Signed-off-by: luowh <luowh@chinasap.cn> 1

--- app/src/main/java/com/example/fangl/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/fangl/notificationtest/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.fangl.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private Button sendNotice;
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this,NotificationActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
         manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notification = new NotificationCompat.Builder(this)
                         .setContentTitle("Notification Title")
@@ -25,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
+//                        .setSound(Uri.fromFile(new File("/system/medio/audio/ringtones/Luna.ogg")))
+                        .setVibrate(new long[]{0,1000,500,1000})
+                        .setAutoCancel(true)
+                        .setLights(Color.YELLOW,1000,1000)
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.bigpicture)))
                         .build();
         sendNotice = (Button)findViewById(R.id.send_notice);
         sendNotice.setOnClickListener(new View.OnClickListener() {
